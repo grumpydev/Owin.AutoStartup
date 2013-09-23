624a680f020261f6d3406c2b36a9db83dd740f96
Steven Robbins <ste.robbins@gmail.com>
2013-09-23 08:37:18 +0100

    missing files

---
+++ src/Owin.AutoStartup/Owin.AutoStartup.Tests/AutoStartupExtensionsFixture.cs
@@ -0,0 +1,41 @@
+﻿namespace Owin.AutoStartup.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Xunit;
+
+    public class AutoStartupExtensionsFixture
+    {
+        [Fact]
+        public void Should_sort_paths_with_largest_number_of_segments_first()
+        {
+            var autoStartups = new IAutoStartup[] { new FakeAutoStartup("/"), new FakeAutoStartup("/foo/bar"), new FakeAutoStartup("/foo") };
+
+            var sorted = autoStartups.SortBySegmentCount().ToArray();
+
+            Assert.Equal("/foo/bar", sorted[0].Path);
+            Assert.Equal("/foo", sorted[1].Path);
+            Assert.Equal("/", sorted[2].Path);
+        } 
+    }
+
+    public class FakeAutoStartup : IAutoStartup
+    {
+        public FakeAutoStartup(string path)
+        {
+            this.Path = path;
+        }
+
+        public string Name { get; private set; }
+
+        public string Path { get; private set; }
+
+        public IDictionary<string, object[]> DefaultBuilderCommands { get; private set; }
+
+        public void Configuration(IAppBuilder builder)
+        {
+            throw new System.NotSupportedException();
+        }
+    }
+}
